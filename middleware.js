156312ee7c0b2db91895b736ffaf6270ae745869
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2025-07-22 21:34:30 +0200

    (seo) Suppression du middleware avec redirection www, car conflit de trop de redirections

--- middleware.js
+++ middleware.js
@@ -1,27 +1,18 @@
 // middleware.js
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const url = request.nextUrl.clone();
+  const acceptLang = request.headers.get('accept-language') || 'fr'
+  const lang = acceptLang.startsWith('fr') ? 'fr' : 'en'
 
-  // 1. Forcer la suppression de "www."
-  if (url.hostname === 'www.hexagone-tours.com') {
-    url.hostname = 'hexagone-tours.com';
-    return NextResponse.redirect(url, 301);
-  }
-
-  // 2. Redirection de la racine vers la langue
-  const acceptLang = request.headers.get('accept-language') || 'fr';
-  const lang = acceptLang.startsWith('fr') ? 'fr' : 'en';
-  const { pathname } = url;
-
+  const { pathname } = request.nextUrl
   if (pathname === '/') {
-    return NextResponse.redirect(new URL(`/${lang}`, request.url));
+    return NextResponse.redirect(new URL(`/${lang}`, request.url))
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/'], // Applique uniquement sur la racine
-};
+  matcher: ['/'], // Important : matcher bien configuré
+}
