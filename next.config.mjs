3b5039e9e037076633128692d4d7a1aeb3c533fc
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2025-09-03 18:05:12 +0200

    (seo) modification next.config.mjs pour obtenir redirection 301 permanente

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,18 @@
 const nextConfig = {
   async redirects() {
     return [
+      // Forcer non-www -> www (géré déjà côté Vercel, mais on le garde pour cohérence)
       {
-        source: '/$',
-        destination: '/',
-        permanent: true,
+        source: '/:path*',
+        has: [{ type: 'host', value: 'hexagone-tours.com' }],
+        destination: 'https://www.hexagone-tours.com/:path*',
+        permanent: true, // 301
+      },
+      // Forcer / -> /fr en 301 permanent
+      {
+        source: '/',
+        destination: '/fr',
+        permanent: true, // 301 permanent (au lieu de 307)
       },
     ];
   },
